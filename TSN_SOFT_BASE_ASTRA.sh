f149559b07b98187e32dbc0895de6c2079682b4f
Talipov S.N <proffix4@gmail.com>
2022-04-15 18:22:00 +0600

    Update TSN_SOFT_BASE_ASTRA.sh

--- TSN_SOFT_BASE_ASTRA.sh
+++ TSN_SOFT_BASE_ASTRA.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt install wget gthumb retext pdfshuffler gtkhash xfburn -y
+sudo apt install wget gthumb pdfshuffler gtkhash xfburn -y
 sudo apt install neofetch -y
