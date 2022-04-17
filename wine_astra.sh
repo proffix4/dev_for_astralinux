7fff706dd3307a7392ba79fa170f41c50de89cac
Talipov S.N <proffix4@gmail.com>
2022-04-18 00:58:39 +0900

    Add files via upload

---
+++ wine_astra.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install wine ia32-libs zenity -y
+
+cd "${HOME}/Downloads"
+wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
+chmod +x winetricks
+sudo mv winetricks /usr/bin/winetricks
+
+winetricks
