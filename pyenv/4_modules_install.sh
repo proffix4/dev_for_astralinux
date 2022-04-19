a7b9f2392d3bce6a57468c0e1b725b79e257738e
Talipov S.N <proffix4@gmail.com>
2022-04-19 13:51:20 +0600

    Add files via upload

---
+++ pyenv/4_modules_install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+curl -sS https://bootstrap.pypa.io/pip/3.5/get-pip.py | sudo python3
+python3 -m pip install --user --upgrade pip
+python3 -m pip install setuptools numpy pandas openpyxl xlrd
+python3 -m pip install python-docx XlsxWriter openpyxl ebooklib pykson bs4
+python3 -m pip install pyinstaller Pillow
+python3 -m pip install wxPython
+python3 -m pip install PyQtWebEngine
+python3 -m pip install PyQt5-sip --upgrade
