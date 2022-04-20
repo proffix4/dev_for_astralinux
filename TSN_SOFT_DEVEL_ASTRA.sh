d7adcd3d2b6cd9737e41b29573bf26071edbcea3
Talipov S.N <proffix4@gmail.com>
2022-04-20 18:19:28 +0600

    Add files via upload

--- TSN_SOFT_DEVEL_ASTRA.sh
+++ TSN_SOFT_DEVEL_ASTRA.sh
@@ -25,8 +25,8 @@ sudo apt install build-essential gdb default-jdk -y
 
 sudo apt install python3 python3-pip python3-pyqt5 python3-pyqt5.qtsql qttools5-dev-tools build-essential python3-tk python3-matplotlib -y
 sudo apt install pyqt5-dev-tools libgtk-3-dev -y
-sudo apt-get install python3-dev -y
-sudo apt-get install libjpeg62-turbo-dev -y
+sudo apt install python3-dev -y
+sudo apt install libjpeg62-turbo-dev -y
 sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
 sudo apt install curl -y
 curl -sS https://bootstrap.pypa.io/pip/3.5/get-pip.py | sudo python3
