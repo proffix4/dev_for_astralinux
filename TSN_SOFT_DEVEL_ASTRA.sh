3eb3f71b8b360c4dbcd2f891a1a5cd04c695ab7a
Talipov S.N <proffix4@gmail.com>
2022-04-20 16:01:15 +0600

    Add files via upload

--- TSN_SOFT_DEVEL_ASTRA.sh
+++ TSN_SOFT_DEVEL_ASTRA.sh
@@ -10,7 +10,9 @@ sudo apt install curl -y
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install java 11.0.12-open
-sdk default java 11.0.12-open
+sdk install java 17.0.2-open
+#sdk default java 11.0.12-open
+sdk default java 17.0.2-open
 
 sudo apt install qemu-kvm android-tools-adb -y
 sudo adduser tsn kvm
