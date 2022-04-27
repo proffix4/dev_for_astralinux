f455ba4abdaa2dbb31ac7355a9099e1cf2217d7e
Talipov S.N <proffix4@gmail.com>
2022-04-27 20:49:16 +0600

    Update TSN_SOFT_BASE_ASTRA.sh

--- УСТАНОВКА/Шаг2 (Общие программы)/TSN_SOFT_BASE_ASTRA.sh
+++ УСТАНОВКА/Шаг2 (Общие программы)/TSN_SOFT_BASE_ASTRA.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install wget gthumb pdfshuffler gtkhash xfburn -y
+sudo apt install gnome-disk-utility -y
 sudo apt install neofetch -y
 sudo apt install snapd -y
